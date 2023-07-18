3020bcf45ea102179d1e6922b2f2f09d52cfc876
yeonseo <yulga_nara@naver.com>
2023-07-18 23:52:49 +0900

    Order 기능 추가

--- src/main/java/com/example/productorderservice/product/Product.java
+++ src/main/java/com/example/productorderservice/product/Product.java
@@ -22,7 +22,7 @@ class Product {
     private int price;
     private DiscountPolicy discountPolicy;
 
-    Product(String name, int price, DiscountPolicy discountPolicy) {
+    public Product(String name, int price, DiscountPolicy discountPolicy) {
         Assert.hasText(name, "상품명은 필수입니다.");
         Assert.isTrue(price > 0, "상품 가격은 0보다 커야합니다.");
         Assert.notNull(discountPolicy, "할인정책은 필수입니다.");
--- src/test/java/com/example/productorderservice/order/OrderServiceTest.java
+++ src/test/java/com/example/productorderservice/order/OrderServiceTest.java
@@ -1,104 +1,19 @@
 package com.example.productorderservice.order;
 
+import com.example.productorderservice.product.DiscountPolicy;
 import com.example.productorderservice.product.Product;
-import com.example.productorderservice.product.ProductRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.Assert;
 
-import java.util.HashMap;
-import java.util.Map;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class OrderServiceTest {
-
-    private OrderService orderService;
-    private OrderPort orderPort;
-    @BeforeEach
-    void setUp() {
-        orderPort = new OrderPort() {};
-        orderService = new OrderService(orderPort);
-    }
+class OrderTest {
 
     @Test
-    void 상품주문() {
-        final Long productId = 1L;
-        final int quantity = 2;
-        final CreateOrderRequest request = new CreateOrderRequest(productId, quantity);
+    void getTotalPrice() {
+        final Order order = new Order(new Product("상품명", 2000, DiscountPolicy.FIX_1000_AMOUNT), 2);
 
-        orderService.createOrder(request);
-    }
+        final int totalPrice = order.getTotalPrice();
 
-    private record CreateOrderRequest (Long productId, int quantity){
-
-        private CreateOrderRequest {
-            Assert.notNull(productId, "상품 ID는 필수입니다.");
-            Assert.isTrue(quantity > 0, "상품 수량은 0보다 커야 합니다.");
-        }
-    }
-
-    private class OrderService {
-        private final OrderPort orderPort;
-
-        private OrderService(OrderPort orderPort) {
-            this.orderPort = orderPort;
-        }
-
-        public void createOrder(CreateOrderRequest request) {
-            final Product product = orderPort.getProductById(request.productId());
-            final Order order = new Order(product, request.quantity());
-
-            orderPort.save(order);
-        }
-    }
-
-    private class OrderPort {
-        private final ProductRepository productRepository;
-        private final OrderRepository orderRepository;
-
-        private OrderPort(ProductRepository productRepository, OrderRepository orderRepository) {
-            this.productRepository = productRepository;
-            this.orderRepository = orderRepository;
-        }
-
-        public Product getProductById(final Long productId) {
-            return productRepository.findById(productId)
-                    .orElseThrow(() -> new IllegalArgumentException("상품이 존재하지 않습니다."));
-        }
-
-        public void save(Order order) {
-            orderRepository.save(order);
-        }
-    }
-
-    private class Order {
-        private Long id;
-        private final Product product;
-        private final int quantity;
-
-        public Order(Product product, int quantity) {
-            Assert.notNull(product, "상품은 필수입니다.");
-            Assert.isTrue(quantity > 0, "상품 수량은 0보다 커야 합니다.");
-
-            this.product = product;
-            this.quantity = quantity;
-        }
-
-        public void assignId(Long id) {
-            this.id = id;
-        }
-
-        public Long getId() {
-            return id;
-        }
-    }
-
-    private class OrderRepository {
-        private final Map<Long, Order> persistence = new HashMap<>();
-        private Long sequence = 0L;
-
-        public void save(Order order) {
-            order.assignId(++sequence);
-            persistence.put(order.getId(), order);
-        }
+        assertThat(totalPrice).isEqualTo(2000);
     }
 }
